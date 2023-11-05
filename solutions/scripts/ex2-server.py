a9b482390194a887a4874508ea80593f81a47d82
Andrey Rastopshin <61227521+KroJIak@users.noreply.github.com>
2023-11-05 17:34:15 +0400

    Update ex2-server.py

--- solutions/scripts/ex2-server.py
+++ solutions/scripts/ex2-server.py
@@ -18,4 +18,4 @@ async def getObject(request: Request):
         return f'[ERROR]: {format_exc()}'
 
 if __name__ == '__main__':
-    uvicorn.run('kerasServer:app', host='0.0.0.0', port=8080, reload=True, log_level='info', workers=5)
+    uvicorn.run('ex2-server:app', host='0.0.0.0', port=8080, reload=True, log_level='info', workers=5)
